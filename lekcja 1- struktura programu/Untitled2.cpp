794f9cef2bc72b1ab12cfc70387337bda4d36aca
fydzyki <ponomarenkods17@gmail.com>
2023-10-25 11:51:28 +0200

    Komentarz

--- lekcja 1- struktura programu/Untitled2.cpp
+++ lekcja 1- struktura programu/Untitled2.cpp
@@ -1,7 +1,22 @@
  #include <iostream>
  using namespace std;
  int main(){
+ 	short liczba = 2; // zmienna liczbowa
+ 	float liczbaUzytkownika;
  	cout <<"Hello world!"<<endl;
- 	cout <<"Imię";
+ 	cout << "Imie " << liczba; // wyswietlenie zmiennej
+ 	
+ 	cout << endl; // koniec linii
+ 	cout << "Podaj liczbe: ";
+ 	cin >> liczbaUzytkownika; // wprowadzanie danych i zapisanie do zmiennej
+ 	
+ 	cout << endl;
+ 	cout << "Podana liczba: " << liczbaUzytkownika;
+ 	
+ 	/*
+ 	komentarz
+ 	na wiele linii
+ 	*/
+ 	
  	return 0;
  }
