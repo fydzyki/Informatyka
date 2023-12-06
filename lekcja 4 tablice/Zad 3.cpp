2e275c9d8856da1d2d181177ca15d389c2ecf175
fydzyki <ponomarenkods17@gmail.com>
2023-12-06 11:54:02 +0100

    Komentarz

---
+++ lekcja 4 tablice/Zad 3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+	int main(){
+		
+		int liczby[5];
+	cout << " Wprowadz 5 liczb dodatnich i ujemnych: ";
+	for (int i = 0; i < 5; i++){
+		cin >> liczby[i];
+	}
+	
+	cout << " Liczby ujemne zamienione na 0: ";
+	if (liczby[i] < 0){
+	if (liczby [i] = 0);
+	} 
+	for (int i = 0; i < 5; ++i){
+		cout<<liczby[i] << " ";
+	}
+	return 0;
+	}
